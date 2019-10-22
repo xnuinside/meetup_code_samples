f42bbd086405bdcf6e6831024b45d1c9b99d913b
xnuinside <xnuinside@gmail.com>
2019-10-22 16:46:18 +0300

    heap size before pympler import profilers_memory_fun.py

--- meetup_examples/profilers_memory_fun.py
+++ meetup_examples/profilers_memory_fun.py
@@ -4,6 +4,10 @@ h = hpy()
 # heap size: 4619038 bytes
 print(h.heap())
 
+
+from pympler.asizeof import asizeof
+
+
 def funct_to_profile():
     a = lambda: 10
     b = lambda: 10
@@ -11,8 +15,10 @@ def funct_to_profile():
     a()
     b()
     big_list_check = [{'1': 1} for _ in range(100000)]
+    print("asizeof ", asizeof(big_list_check))
     print(h.heap())
     c = big_list_check
+
 # imports must be here to check heap size step by step
 from memory_profiler import profile
 # heap size: 7085995 bytes
